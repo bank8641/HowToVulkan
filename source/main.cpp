c88280a208b9015fefdce3020309c5f9e2d0484c
Charles Giessen <46324611+charles-lunarg@users.noreply.github.com>
2026-01-16 00:30:04 -0600

    Fix linux build (#11)
    * Add non-windows build support to CMake
    
    * Remove whitespace

--- source/main.cpp
+++ source/main.cpp
@@ -77,7 +77,7 @@ struct ShaderDataBuffer {
 std::array<ShaderDataBuffer, maxFramesInFlight> shaderDataBuffers;
 struct Texture {
 	VmaAllocation allocation{ VK_NULL_HANDLE };
-	VkImage image{ VK_NULL_HANDLE };	
+	VkImage image{ VK_NULL_HANDLE };
 	VkImageView view{ VK_NULL_HANDLE };
 	VkSampler sampler{ VK_NULL_HANDLE };
 };
@@ -227,7 +227,7 @@ int main(int argc, char* argv[])
 		Vertex v{
 			.pos = { attrib.vertices[index.vertex_index * 3], -attrib.vertices[index.vertex_index * 3 + 1], attrib.vertices[index.vertex_index * 3 + 2] },
 			.normal = { attrib.normals[index.normal_index * 3], -attrib.normals[index.normal_index * 3 + 1], attrib.normals[index.normal_index * 3 + 2] },
-			.uv = { attrib.texcoords[index.texcoord_index * 2], 1.0 - attrib.texcoords[index.texcoord_index * 2 + 1] } 
+			.uv = { attrib.texcoords[index.texcoord_index * 2], 1.0 - attrib.texcoords[index.texcoord_index * 2 + 1] }
 		};
 		vertices.push_back(v);
 		indices.push_back(indices.size());
@@ -453,7 +453,7 @@ int main(int argc, char* argv[])
 		for (auto i = 0; i < 3; i++) {
 			auto instancePos = glm::vec3((float)(i - 1) * 3.0f, 0.0f, 0.0f);
 			shaderData.model[i] = glm::translate(glm::mat4(1.0f), instancePos) * glm::mat4_cast(glm::quat(objectRotations[i]));
-		}		
+		}
 		memcpy(shaderDataBuffers[frameIndex].mapped, &shaderData, sizeof(ShaderData));
 		// Build command buffer
 		auto cb = commandBuffers[frameIndex];
